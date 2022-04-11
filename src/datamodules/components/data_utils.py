983365f050e23f52e2a95563c585e1c7420dc003
Your Name <mahdi.gilany@queensu.ca>
2022-04-11 15:00:54 -0400

    datamodule

---
+++ src/datamodules/components/data_utils.py
@@ -0,0 +1,170 @@
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+from einops import rearrange
+from mat73 import loadmat as loadmat73
+from scipy.io import matlab
+from sklearn.model_selection import GroupShuffleSplit
+
+
+def load_matlab(filename):
+    with open(filename, 'rb') as fp:
+        return matlab.loadmat(fp, struct_as_record=False, squeeze_me=True)
+
+
+def load_matlab73(filename):
+    return loadmat73(filename)
+
+
+def load_pickle(filename):
+    with open(filename, 'rb') as fp:
+        return pickle.load(fp)
+
+
+def to_categorical(y):
+    n_classes = np.max(y) + 1
+    y_c = np.zeros((len(y), np.int(n_classes)))
+    for i in range(len(y)):
+        y_c[i, np.int(y[i])] = 1
+    return y_c.astype(int)
+
+
+def merge_split_train_val(meta_data, random_state=26, train_val_split=0.25):
+    """
+
+      :param meta_data:
+      :param random_state:
+      :param train_val_split:
+      :return:
+      """
+
+    # merge train-val then randomize patient ID to split train-val again
+    gs = list(meta_data["GS_train"]) + list(meta_data['GS_val'])
+    pid = list(meta_data["PatientId_train"]) + list(meta_data['PatientId_val'])
+    inv = list(meta_data["inv_train"]) + list(meta_data['inv_val'])
+
+    df1 = pd.DataFrame({'pid': pid, 'gs': gs, 'inv': inv})
+    df1 = df1.assign(condition=df1.gs.replace({'Benign': 'Benign', 'G7': 'Cancer', 'G8': 'Cancer',
+                                               'G9': 'Cancer', 'G10': 'Cancer'}))
+
+    train_inds, test_inds = next(GroupShuffleSplit(test_size=train_val_split, n_splits=2,
+                                                   random_state=random_state).split(df1, groups=df1['pid']))
+    df1 = df1.assign(group='train')
+    df1.loc[test_inds, 'group'] = 'val'
+    df1 = df1.sort_values(by='pid')
+
+    pid_tv = {
+        'train': df1[df1.group == 'train'].pid.unique(),
+        'val': df1[df1.group == 'val'].pid.unique(),
+    }
+
+    # Merge train - val
+    keys = [f[:-4] for f in meta_data.keys() if 'val' in f]
+    merge = {}
+    for k in keys:
+        if isinstance(meta_data[f'{k}_train'], list):
+            merge[k] = meta_data[f'{k}_train'] + meta_data[f'{k}_val']
+        else:
+            merge[k] = np.concatenate([meta_data[f'{k}_train'], meta_data[f'{k}_val']], axis=0)
+
+    # Initialize the new meta_data
+    target = {}
+    for set_name in ['train', 'val']:
+        for k in keys:
+            target[f'{k}_{set_name}'] = []
+
+    # Re-split data into two sets based on randomized patient ID
+    for i, pid in enumerate(merge['PatientId']):
+        for set_name in ['train', 'val']:
+            if pid in pid_tv[set_name]:
+                for k in keys:
+                    k_target = f'{k}_{set_name}'
+                    target[k_target].append(merge[k][i])
+
+    # Assign to original input data after finishing creating a new one
+    for set_name in ['train', 'val']:
+        for k in keys:
+            k_target = f'{k}_{set_name}'
+            meta_data[k_target] = target[k_target]
+            if isinstance(merge[k], np.ndarray):
+                meta_data[k_target] = np.array(target[k_target]).astype(np.ndarray)
+
+    return meta_data
+
+
+def remove_empty_lowinv_data(meta_data, dataset_hyp=None):
+    """
+    :param meta_data:
+    :return:
+    """
+
+    inv_cutoff = dataset_hyp.inv_cutoff
+
+    for set_name in ['val', 'test', 'train']:
+        data = meta_data[f"data_{set_name}"]
+        inv = meta_data[f"inv_{set_name}"]
+
+        # indexes to include: not empty data and with inv bigger that cutoff
+        include_idx = [i for i, d in enumerate(data)
+                       if (isinstance(d, np.ndarray) and (inv[i] >= inv_cutoff or inv[i] == 0.))]
+
+        for k in meta_data.keys():
+            if set_name in k:
+                if isinstance(meta_data[k], list):
+                    meta_data[k] = [_ for i, _ in enumerate(meta_data[k]) if i in include_idx]
+                else:
+                    meta_data[k] = meta_data[k][include_idx]
+    return meta_data
+
+
+def estimate_patchcenter(meta_data, dataset_hyp=None):
+    """finds patch centers considering that only 1mmx1mm patches are accessible.
+
+    Inspired from Amoon's matlab code
+    """
+    patch_sz = dataset_hyp.patch_sz
+    jump_sz = dataset_hyp.jump_sz
+
+    centers = meta_data['patch_centers1x1']
+    needle_mask = meta_data['needle_mask']
+
+    # changing dimensionality of patch centers
+    centers = rearrange(centers, '(ax c) lat -> ax c lat', ax=28)
+
+    # finding if center of a patch is inside needle mask
+    patch_mask = needle_mask[centers[:, 0, 0][:, None], centers[:, :, 1]]
+
+    # finding lateral index of central patch
+    col_select = np.sum(patch_mask, axis=0)
+    ind_lat = np.argwhere(col_select > 0)
+    st_lat = ind_lat[0].item() + np.floor((patch_sz - 1) / 2.).astype(int)
+    end_lat = ind_lat[-1].item() - np.ceil((patch_sz - 1) / 2.).astype(int)
+
+    patch_centers_sl = []
+    patch_center_ind = []
+    for i in range(st_lat, end_lat + 1, jump_sz):
+        # finding axial index of central patch
+        ind_ax = np.argwhere(patch_mask[:, i] > 0)
+        j = np.floor(ind_ax.mean()).astype(int)
+
+        # finding actual patch center from indices and saving it
+        patch_center_ind.append(np.array([[j, i]]))
+        cent = np.array([[centers[j, 0, 0], centers[0, i, 1]]])
+        patch_centers_sl.append(cent)
+
+    patch_centers_sl = np.concatenate(patch_centers_sl)
+    patch_center_ind = np.concatenate(patch_center_ind)
+    return patch_centers_sl
+
+
+def get_data_roots(data_dir):
+    """finds roots of all data."""
+    # list_all_files = []
+    list_all_roots = []
+    for root, dir, files in os.walk(data_dir):
+        # file_dir = [root+'\\'+ f for i, f in enumerate(files)]
+        # list_all_files.append(file_dir)
+        list_all_roots.append(root)
+    return list_all_roots
---
+++ src/datamodules/components/datasets.py
@@ -0,0 +1,225 @@
+from itertools import product
+
+import numpy as np
+from ismember import ismember
+from PIL import Image
+from pytorch_lightning.core.mixins import HyperparametersMixin
+from torch.utils.data import Dataset
+
+from .data_utils import *
+
+
+def resize_norm(patch):
+    patch_resized = np.array(Image.fromarray(patch).resize((256, 256)))
+    return (patch_resized - np.mean(patch_resized, keepdims=True))/np.std(patch_resized, keepdims=True)
+    # return patch_resized
+
+
+class ExactDataset(Dataset, HyperparametersMixin):
+    """Characterizes a dataset for PyTorch."""
+
+    def __init__(
+            self,
+            state: str,
+            dataset_hyp: dict,
+            extended_metadata: dict):
+        super().__init__()
+
+        """
+        # save all input parameters in hparams including:
+        #   # data_dir = dataset_hyp.data_dir
+        #   # inv_cutoff = dataset_hyp.inv_cutoff
+        #   # patch_sz = dataset_hyp.patch_sz
+        #   # jump_sz = dataset_hyp.jump_sz
+
+        #   # extended_metadata.meta_data
+        #   # extended_metadata.patch_centers_sl
+        #   # extended_metadata.data_roots
+        """
+        self.save_hyperparameters(logger=False)
+
+        # patch centers of the whole RF image. List [axials, laterals]
+        patch_centers_RFimg = extended_metadata.meta_data['patch_centers1x1']
+        self.patch_centers_RFimg: list = [np.unique(patch_centers_RFimg[:, 0]), np.unique(patch_centers_RFimg[:, 1])]
+
+        # finding len and name of patch centers in out dataset based on patch centers inside
+        # needle for SL and inside the whole image in SSL (former not implemented yet)
+        self.len_ds, self.used_patch_names, self.all_corelen_sl = self.find_len_centers()
+
+        # RF image index number for each of used patches (used_patch_names). These indexes can be used for
+        # accessing information in meta data corresponding to train/test/val patches
+        self.ind_RFimg = [self.patchind_to_RFimgind(i) for i, n in enumerate(self.used_patch_names)]
+
+        # labels
+        label = extended_metadata.meta_data[f'label_{self.hparams.state}']
+        label = [label[ind] for i, ind in enumerate(self.ind_RFimg)]
+        self.labels = to_categorical(label)
+
+
+
+    def find_len_centers(self):
+        """finds the names of central patches and len of that which correspond to len of data since
+        for each central patch we would have one bigger patch."""
+        state = self.hparams.state
+        meta_data = self.hparams.extended_metadata.meta_data
+
+        data_names = meta_data[f'data_{state}']
+
+        # appending name of all central patches (1x1 patches) inside needle and prostate from RF images in data_{state}
+        all_patchnames_sl_pr = []
+        for i, patch1x1_name_pr in enumerate(data_names):
+            """looping over all RF images and multiplying mask of prostate & needle and selecting
+            centers of 5x5 patches."""
+            # first get all centers of patches inside prostate for i'th RF image
+            all_1x1patchcenters_pr = [np.array(name.split('_')[-3:-1]).astype(int) for name in patch1x1_name_pr]
+            all_1x1patchcenters_pr = np.stack(all_1x1patchcenters_pr)
+
+            # first get all names of patches inside prostate for i'th RF image
+            ## todo: the format of files are set here. Needs to change.
+            all_1x1patchnames_pr = [np.array('_'.join(name.split('_')[:-1]) + '_frm1.pkl') for name in patch1x1_name_pr]
+            all_1x1patchnames_pr = np.stack(all_1x1patchnames_pr)
+
+            # retaining only patches inside needle (inside extended_metadata.patch_centers_sl)
+            ind, _ = ismember(all_1x1patchcenters_pr, self.hparams.extended_metadata.patch_centers_sl, "rows")
+            #   #validate if bigger patches are inside prostate as well, in addition to central 1x1 patches.
+            ind = self.validate_mask_ind(all_1x1patchcenters_pr, ind)
+            all_patchnames_sl_pr.append(all_1x1patchnames_pr[ind])
+
+        # corelen for all cores in supervised learning setting (number of patches in each core)
+        all_corelen_sl = [core.shape[0] for core in all_patchnames_sl_pr]
+
+        return np.sum(all_corelen_sl), np.concatenate(all_patchnames_sl_pr), all_corelen_sl
+
+    def validate_mask_ind(self, all_1x1patchcenters_pr, ind):
+        """checking head and tail big patches to see if they are completely located in prostate
+        region corner patches of head and tail patches are only checked."""
+
+        # patch size for finding the corner of bigger patch
+        patch_sz = self.hparams.dataset_hyp.patch_sz
+
+        # axial and lateral numbers corresponding to patch centers in the whole RF image (used to find corner patches)
+        patch_centers_RFimg = self.patch_centers_RFimg
+        axl_center_numbers = patch_centers_RFimg[0]
+        lat_center_numbers = patch_centers_RFimg[1]
+
+        # centers of central patches inside both needle and prostate
+        all_patchcenters_sl_pr = all_1x1patchcenters_pr[ind]
+
+        # patches to be checked
+        chk_centers = range(len(all_patchcenters_sl_pr))
+        # chk_centers = [0, 1, 2, -1, -2, -3]
+        # if there is no or less than 3 patches inside both prostate and needle masks
+        # if len(all_patchcenters_sl_pr) <= 5:
+        #     chk_centers = list(range(len(all_patchcenters_sl_pr)))
+
+        # span of axial and lateral indices
+        hlf_patch_sz = np.floor((patch_sz-1)/2.).astype(int)
+        hlf_patch_sz_rest = np.ceil((patch_sz-1)/2.).astype(int)
+
+        copy_ind = np.copy(ind)
+        for center in chk_centers:
+            """checking head and tail big patches to see if they are completely located in prostate
+            region."""
+
+            # indexes of possible axial and lateral numbers
+            cent_axl_ind, = np.where(axl_center_numbers == all_patchcenters_sl_pr[center, 0])
+            cent_lat_ind, = np.where(lat_center_numbers == all_patchcenters_sl_pr[center, 1])
+
+            # axial and lateral numbers of left bottom corner
+            ## todo: this redundancy should be modified
+            corner_axl = axl_center_numbers[cent_axl_ind+hlf_patch_sz_rest][0]
+            corner_lat = lat_center_numbers[cent_lat_ind-hlf_patch_sz][0]
+            member_bool1 = ismember(np.array([[corner_axl, corner_lat]]), all_1x1patchcenters_pr, "rows")[0]
+
+            # axial and lateral numbers of top right corner
+            corner_axl = axl_center_numbers[cent_axl_ind-hlf_patch_sz][0]
+            corner_lat = lat_center_numbers[cent_lat_ind+hlf_patch_sz_rest][0]
+            member_bool2 = ismember(np.array([[corner_axl, corner_lat]]), all_1x1patchcenters_pr, "rows")[0]
+
+            # axial and lateral numbers of bottom right corner
+            corner_axl = axl_center_numbers[cent_axl_ind+hlf_patch_sz_rest][0]
+            corner_lat = lat_center_numbers[cent_lat_ind+hlf_patch_sz_rest][0]
+            member_bool3 = ismember(np.array([[corner_axl, corner_lat]]), all_1x1patchcenters_pr, "rows")[0]
+
+            # axial and lateral numbers of top left corner
+            corner_axl = axl_center_numbers[cent_axl_ind-hlf_patch_sz][0]
+            corner_lat = lat_center_numbers[cent_lat_ind-hlf_patch_sz][0]
+            member_bool4 = ismember(np.array([[corner_axl, corner_lat]]), all_1x1patchcenters_pr, "rows")[0]
+
+            # if any of the corners are outside prostate, change the index of that patch to be False rather than True
+            if (not member_bool1) or (not member_bool2) or (not member_bool3) or (not member_bool4):
+                ind_of_ind, = np.where(ind == True)
+                copy_ind[ind_of_ind[center]] = False
+
+        return copy_ind
+
+    def __len__(self):
+        return self.len_ds
+
+    def __getitem__(self, index):
+        x_patch, y_target = self.get_img_target(index)
+
+        return x_patch, y_target
+
+    def get_img_target(self, index):
+        # name of central patch that should be used for creating the whole patch
+        patch_name = self.used_patch_names[index]
+
+        # finding patch root
+        patch_parts = patch_name.split('_')
+        patch_root = '_'.join(patch_parts[:-3])
+        centers_centralpatch = np.array(patch_parts[-3:-1]).astype(int)
+
+        # all roots of data
+        data_roots = self.hparams.extended_metadata.data_roots
+
+        # getting the complete patch root
+        complete_fileroot = [f for f in data_roots if patch_root in f]
+        complete_fileroot = os.path.join(complete_fileroot[0], patch_root)
+
+        # loading and creating the patch
+        x_patch = self.load_create_patch(complete_root=complete_fileroot, centers_centralpatch=centers_centralpatch,
+                                         suffix=patch_parts[-1])
+
+        # assigning core label to patch label
+        y_label = self.labels[index]
+        return x_patch, y_label
+
+    def load_create_patch(self, complete_root, centers_centralpatch, suffix):
+        # patch size for creating bigger patch
+        patch_sz = self.hparams.dataset_hyp.patch_sz
+        patch_centers_RFimg = self.patch_centers_RFimg
+
+
+        # finding index of cental patch in the RF img
+        axl_center_numbers = patch_centers_RFimg[0]
+        lat_center_numbers = patch_centers_RFimg[1]
+        cent_axl_ind, = np.where(axl_center_numbers == centers_centralpatch[0])
+        cent_lat_ind, = np.where(lat_center_numbers == centers_centralpatch[1])
+
+        # span of axial and lateral indices
+        hlf_patch_sz = np.floor((patch_sz-1)/2.).astype(int)
+        hlf_patch_sz_rest = np.ceil((patch_sz-1)/2.).astype(int)
+        cent_axl_range_ind = np.arange(cent_axl_ind[0]-hlf_patch_sz, cent_axl_ind[0]+hlf_patch_sz_rest+1)
+        cent_lat_range_ind = np.arange(cent_lat_ind[0]-hlf_patch_sz, cent_lat_ind[0]+hlf_patch_sz_rest+1)
+
+        all_1x1patches = []
+        for axl_ind, lat_ind in product(cent_axl_range_ind,cent_lat_range_ind):
+            patch_name = complete_root + f'_{axl_center_numbers[axl_ind]}_{lat_center_numbers[lat_ind]}_' + suffix
+            all_1x1patches.append(load_pickle(patch_name)) #['data'])
+
+        # stitching patches together
+        all_1x1patches = np.stack(all_1x1patches)
+        all_1x1patches = rearrange(all_1x1patches, '(b1 b2) h w -> (b1 h) (b2 w)', b1=patch_sz) #25x360x11 to 5x360 x 5x11
+        return resize_norm(all_1x1patches)
+
+    def patchind_to_RFimgind(self, index):
+        # loading all corelen
+        corelen_sl = self.all_corelen_sl
+
+        # cumulative sum of corelen to find where index is located
+        cumsum_corelen = np.cumsum(corelen_sl)
+        ind, = np.where(index <= cumsum_corelen)
+        RFimg_ind = ind[0]
+
+        return RFimg_ind
