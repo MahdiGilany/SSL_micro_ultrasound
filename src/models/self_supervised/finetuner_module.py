e477e4bc0a097e11e133480a4543a2096e3ff7c1
Paul Wilson <pfrwilson@gmail.com>
2022-06-16 15:02:15 -0400

    added option to instantiate finetuner with no ckpt

--- src/models/self_supervised/finetuner_module.py
+++ src/models/self_supervised/finetuner_module.py
@@ -1,29 +1,37 @@
+from typing import Optional
 import torch
 import torch.nn.functional as F
 from pl_bolts.models.self_supervised.ssl_finetuner import SSLFineTuner
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
-from torchmetrics import Accuracy, MaxMetric, MetricCollection, StatScores, ConfusionMatrix, AUROC
+from torchmetrics import (
+    Accuracy,
+    MaxMetric,
+    MetricCollection,
+    StatScores,
+    ConfusionMatrix,
+    AUROC,
+)
+from warnings import warn
 
 
 class ExactFineTuner(SSLFineTuner):
     """
     This class implements finetunjng ssl module. It attaches a neural net on top and trains it.
     """
+
     def __init__(
-            self,
-            backbone: torch.nn.Module,
-            ckpt_path: str,
-            semi_sup: bool = False,
-            batch_size: int = 32,
-            epochs: int = 100,
-            **kwargs
+        self,
+        backbone: torch.nn.Module,
+        ckpt_path: Optional[str] = None,
+        semi_sup: bool = False,
+        batch_size: int = 32,
+        epochs: int = 100,
+        **kwargs
     ):
         super(ExactFineTuner, self).__init__(backbone=backbone, **kwargs)
 
         # whether to do semi supervised or not
         self.semi_sup = semi_sup
-        if self.semi_sup:
-            self.backbone.train()
 
         self.warmup_epochs = 10
         self.warmup_start_lr = 0.0
@@ -31,32 +39,49 @@ class ExactFineTuner(SSLFineTuner):
         self.scheduler_interval = "step"
         self.batch_size = batch_size
         self.max_epochs = epochs
-        self.num_classes = kwargs['num_classes']
+        self.num_classes = kwargs["num_classes"]
 
-        self.backbone = backbone.load_from_checkpoint(ckpt_path, strict=False)
+        self.backbone = backbone
+        if ckpt_path:
+            self.backbone = backbone.load_from_checkpoint(ckpt_path, strict=False)
+        else:
+            warn(
+                "You are using the finetuner model with no loadable checkpoint. The model will be randomly initialized."
+            )
 
         # for memorizing all logits
         self.all_val_online_logits = []
         self.all_test_online_logits = []
 
         # # metrics for logging
-        metrics = MetricCollection({
-            # 'finetune_acc': Accuracy(num_classes=self.num_classes, multiclass=True),
-            'finetune_acc_macro': Accuracy(num_classes=self.num_classes, average='macro', multiclass=True),
-            # 'finetune_auc': AUROC(num_classes=self.num_classes),
-        })
+        metrics = MetricCollection(
+            {
+                # 'finetune_acc': Accuracy(num_classes=self.num_classes, multiclass=True),
+                "finetune_acc_macro": Accuracy(
+                    num_classes=self.num_classes, average="macro", multiclass=True
+                ),
+                # 'finetune_auc': AUROC(num_classes=self.num_classes),
+            }
+        )
         self.train_acc = Accuracy()
-        self.val_metrics = metrics.clone(prefix='val/')
-        self.test_metrics = metrics.clone(prefix='test/')
+        self.val_metrics = metrics.clone(prefix="val/")
+        self.test_metrics = metrics.clone(prefix="test/")
+
+    def on_train_epoch_start(self) -> None:
+        """Changing model to eval() mode has to happen at the
+        start of every epoch, and should only happen if we are not in semi-supervised mode
+        """
+        if not self.semi_sup:
+            self.backbone.eval()
 
     def shared_step(self, batch):
         x, y = batch
 
         if self.semi_sup:
-            feats = self.backbone(x)['feats']
+            feats = self.backbone(x)["feats"]
         else:
             with torch.no_grad():
-                feats = self.backbone(x)['feats']
+                feats = self.backbone(x)["feats"]
 
         feats = feats.view(feats.size(0), -1)
         logits = self.linear_layer(feats)
@@ -68,14 +93,32 @@ class ExactFineTuner(SSLFineTuner):
         loss, logits, y = self.shared_step(batch)
         self.train_acc(logits.softmax(-1), y)
 
-        self.log("train/finetune_loss", loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
-        self.log("train/finetune_acc", self.train_acc.compute(), on_step=False, on_epoch=True, prog_bar=True,
-                 sync_dist=True)
+        self.log(
+            "train/finetune_loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            sync_dist=True,
+        )
+        self.log(
+            "train/finetune_acc",
+            self.train_acc.compute(),
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            sync_dist=True,
+        )
         return loss
 
     def validation_step(self, batch, batch_idx, dataloader_idx: int):
         loss, logits, y = self.shared_step(batch)
-        kwargs = {'on_step': False, 'on_epoch': True, 'sync_dist': True, 'add_dataloader_idx': False}
+        kwargs = {
+            "on_step": False,
+            "on_epoch": True,
+            "sync_dist": True,
+            "add_dataloader_idx": False,
+        }
 
         if dataloader_idx == 0:
             self.all_val_online_logits.append(logits)
@@ -119,8 +162,14 @@ class ExactFineTuner(SSLFineTuner):
         return self._num_training_steps
 
     def configure_optimizers(self):
-        opt_params = [{"params":self.linear_layer.parameters()}, {"params":self.backbone.parameters()}]\
-           if self.semi_sup else self.linear_layer.parameters()
+        opt_params = (
+            [
+                {"params": self.linear_layer.parameters()},
+                {"params": self.backbone.parameters()},
+            ]
+            if self.semi_sup
+            else self.linear_layer.parameters()
+        )
         optimizer = torch.optim.Adam(
             opt_params,
             lr=self.learning_rate,
@@ -129,7 +178,9 @@ class ExactFineTuner(SSLFineTuner):
 
         # set scheduler
         if self.scheduler_type == "step":
-            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, self.decay_epochs, gamma=self.gamma)
+            scheduler = torch.optim.lr_scheduler.MultiStepLR(
+                optimizer, self.decay_epochs, gamma=self.gamma
+            )
         elif self.scheduler_type == "cosine":
             scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
                 optimizer, self.epochs, eta_min=self.final_lr  # total epochs to run
@@ -140,7 +191,9 @@ class ExactFineTuner(SSLFineTuner):
                     optimizer,
                     warmup_epochs=self.warmup_epochs * self.num_training_steps,
                     max_epochs=self.max_epochs * self.num_training_steps,
-                    warmup_start_lr=self.warmup_start_lr if self.warmup_epochs > 0 else self.learning_rate,
+                    warmup_start_lr=self.warmup_start_lr
+                    if self.warmup_epochs > 0
+                    else self.learning_rate,
                     eta_min=self.final_lr,
                 ),
                 "interval": self.scheduler_interval,
