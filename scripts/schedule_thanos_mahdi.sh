e3c8f9196dbd6224b56f92919cc353f583f63fbb
Your Name <mahdi.gilany@queensu.ca>
2022-06-17 16:54:39 -0400

    bash script for Thanos exps (Mahdi)

--- scripts/schedule_thanos_mahdi.sh
+++ scripts/schedule_thanos_mahdi.sh
@@ -2,13 +2,16 @@
 # Shedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
 
-Thanos_original_work_dir=/home/mgilani/data/Exact
+export DATA=/home/mgilani/data/Exact/Exact
+echo "Data is from" ${DATA}
+
+Thanos_original_work_dir=/home/mgilani/data/Exact/Exact
 
 #python train.py data_dir=${Thanos_original_work_dir} experiment=exact_vicreg.yaml datamodule.num_workers=8
 
 # finetune
 #python train.py -m data_dir=${Thanos_original_work_dir} experiment=exact_finetune.yaml datamodule.num_workers=8 seed=13 datamodule.split_randomstate=5,26
-#python train.py data_dir=${Thanos_original_work_dir} experiment=exact_vicreg_UVA600.yaml datamodule.num_workers=8
-python train.py data_dir=${Thanos_original_work_dir} experiment=exact_finetune_UVA600.yaml datamodule.num_workers=8
+python train.py data_dir=${Thanos_original_work_dir} experiment=exact_vicreg_UVA600_mahdi.yaml datamodule.num_workers=8
+#python train.py data_dir=${Thanos_original_work_dir} experiment=exact_finetune_UVA600.yaml datamodule.num_workers=8
 
 #python train.py trainer.max_epochs=10 loggerv=cs
