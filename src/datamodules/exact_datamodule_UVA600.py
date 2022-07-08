82a8cf78501264be4cf787fdb222d4351bc112ae
Your Name <mahdi.gilany@queensu.ca>
2022-07-08 12:24:46 -0400

    multicenter datamodule code debugged. Now works on multiple centers.

--- src/datamodules/exact_datamodule_UVA600.py
+++ src/datamodules/exact_datamodule_UVA600.py
@@ -9,7 +9,7 @@ class ExactDataModuleUVA600(ExactSSLDataModule):
         val_loader = super(ExactDataModuleUVA600, self).val_dataloader()
         test_asval_loader = super(ExactDataModuleUVA600, self).test_dataloader()
 
-        val_loader = val_loader if isinstance(val_loader.dataset, list) else [val_loader]
+        val_loader = val_loader if isinstance(val_loader, list) else [val_loader]
         test_asval_loader = test_asval_loader if isinstance(test_asval_loader, list) else [test_asval_loader]
 
         return val_loader + test_asval_loader
